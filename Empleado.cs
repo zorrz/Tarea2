532f72ed3bb1d54ef3e03ac916fbd8fe6c5ee803
zorrz <jcarcachepayan@gmail.com>
2024-05-05 21:33:13 -0600

    Primer cambio

---
+++ Empleado.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Empleado
+    {
+        private string nombre;
+        private int numeroEmpleado;
+        private decimal salario;
+        private bool activo;
+
+       
+        public Empleado(string nombre, int numeroEmpleado, decimal salario, bool activo = true)
+        {
+            this.nombre = nombre;
+            this.numeroEmpleado = numeroEmpleado;
+            this.salario = salario;
+            this.activo = activo;
+        }
+
+        public void MostrarDetalles()
+        {
+            string estado = (activo) ? "Activo" : "Inactivo";
+            Console.WriteLine($"Nombre: {nombre}, Numero de Empleado: {numeroEmpleado}, Salario: {salario:C}, Estado: {estado}");
+        }
+        public int ObtenerNumeroEmpleado()
+        {
+            return this.numeroEmpleado;
+        }
+
+        public void CambiarEstado(int nuevoEstado)
+        {
+            if (nuevoEstado ==1)
+            {
+                activo = true ;
+                Console.WriteLine("Empleado activado.");
+
+            }
+            else if(nuevoEstado ==0)
+            { 
+                activo= false ;
+                Console.WriteLine("Empleado desactivado.");
+
+            }
+            else
+            {
+                Console.WriteLine("Estado no valido.");
+            }
+        }
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplicacion de Gestion de Empleados");
+
+            List<Empleado> listaEmpleados = new List<Empleado>();
+
+            listaEmpleados.Add(new Empleado("Juan", 101, 30000));
+            listaEmpleados.Add(new Empleado("Maria", 102, 35000));
+            listaEmpleados.Add(new Empleado("Pedro", 103, 32000, false));
+
+
+            foreach (var empleado in listaEmpleados)
+            {
+                empleado.MostrarDetalles();
+            }
+
+            int numeroEmpleadoCambiarEstado = 103;
+            Empleado empleadoCambiarEstado = BuscarEmpleado(listaEmpleados, numeroEmpleadoCambiarEstado);
+
+            if (empleadoCambiarEstado != null)
+            {
+                empleadoCambiarEstado.CambiarEstado(1);
+                empleadoCambiarEstado.MostrarDetalles();
+
+            }
+            else
+            {
+                Console.WriteLine($"Empleado con numero {numeroEmpleadoCambiarEstado} no encontrado.");
+            }
+            
+
+            static Empleado BuscarEmpleado(List<Empleado> empleados, int numeroEmpleado)
+            {
+                foreach(var empleado in empleados)
+                {
+                    if(empleado.ObtenerNumeroEmpleado() == numeroEmpleado)
+                    {
+                        return empleado;
+                    }
+                }
+                return null;
+
+            }
+        }   
+    }
+}
